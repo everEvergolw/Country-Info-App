9247c25952a1345ae800f3df1979d8a30ccb3e53
TomatoKiller <55655626+everEvergolw@users.noreply.github.com>
2023-10-19 22:16:50 +0100

    UH

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const fetch = require('node-fetch');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));

ceb60f1861d5c8c70018d331f9b3a1f955607d49
TomatoKiller <55655626+everEvergolw@users.noreply.github.com>
2023-10-19 21:33:07 +0100

    Update Heroku

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
@@ -70,9 +70,9 @@ app.post('/home_list', async (req, res) => {
 
 
 // Server Start
-const port = process.env.PORT || DEFAULT_PORT; // Note: PORT should be in uppercase
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+const PORT = process.env.PORT || DEFAULT_PORT; // Note: PORT should be in uppercase
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
 
 // At the end of your server.js

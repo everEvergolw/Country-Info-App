a76ad0d0a345988a09fce70cdc5cee0ee4b32362
TomatoKiller <55655626+everEvergolw@users.noreply.github.com>
2023-10-20 12:32:21 +0100

    Update cypress

--- frontend/cypress/integration/countryExplorer.spec.js
+++ frontend/cypress/integration/countryExplorer.spec.js
@@ -23,18 +23,6 @@ describe('Country Explorer App', () => {
       cy.contains('Country name can only contain English letters and spaces').should('be.visible'); 
     });
   
-    it('shows network error when there is a backend issue', () => {
-      cy.server();
-      cy.route({
-        method: 'GET',
-        url: '/api/endpoint/*', // Replace this with your API endpoint
-        status: 500,
-        response: {},
-      });
-      cy.get('input').type('Japan'); 
-      cy.get('button').contains('Search').click();
-      cy.contains('Network error or unexpected issue occurred!').should('be.visible');
-    });
   
     // CountryInfo component tests
     it('loads CountryInfo component on valid input', () => {
@@ -52,27 +40,18 @@ describe('Country Explorer App', () => {
       cy.contains('Geographical Information').should('be.visible');
       cy.get('.geographical-info').within(() => {
         cy.get('.title').contains('Capital').next().should('not.be.empty');
-        // ... more assertions for other fields
       });
     });
   
     it('can expand additional details', () => {
       cy.get('button.showMoreLess').click();
       cy.contains('Languages:').should('be.visible');
-      // ... more assertions for other fields in the AdditionalDetails component
     });
   
-    // This test showcases how you could test an external component behavior. 
     it('can view the country in Google Maps', () => {
       cy.get('a[target="_blank"]').should('have.attr', 'href').and('include', 'google.com/maps');
     });
   
-    // And many more... You'd typically write individual tests for different components and interactions
-    
-    // After all tests
-    after(() => {
-      // Useful to check screenshots or videos in case of failures
-      // Located by default in cypress/screenshots and cypress/videos
-    });
+   
   });
   
